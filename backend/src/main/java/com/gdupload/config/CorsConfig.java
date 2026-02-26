d8dcec61c8402aabf005cb4e9472ab0aa71fa16b
mbfczzzz <a317242392@163.com>
2026-02-26 22:01:21 +0800

    2

--- backend/src/main/java/com/gdupload/config/CorsConfig.java
+++ backend/src/main/java/com/gdupload/config/CorsConfig.java
@@ -2,7 +2,6 @@ package com.gdupload.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -33,10 +32,5 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
